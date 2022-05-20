15c704ab47164c5af7205952e109a3bbc8bc5287
dohung <dohung@localhost>
2022-05-20 14:11:32 +0700

    change DB sql

--- quanlynhansu.sql
+++ quanlynhansu.sql
@@ -1,73 +1,306 @@
-CREATE TABLE `chucvu` (
-  `MaCV` varchar(255) PRIMARY KEY,
-  `TenCV` varchar(255),
-  `GhiChu` varchar(255)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 20, 2022 at 09:09 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.27
 
-CREATE TABLE `khenthuongkyluat` (
-  `MaKTKL` varchar(255),
-  `MaNV` varchar(255),
-  PRIMARY KEY (`MaKTKL`, `MaNV`)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `luong` (
-  `MaLuong` varchar(255),
-  `MaNV` varchar(255),
-  PRIMARY KEY (`MaLuong`, `MaNV`)
-);
 
-CREATE TABLE `nhanvien` (
-  `MaNV` varchar(255) PRIMARY KEY,
-  `HinhNV` varchar(255),
-  `TenNV` varchar(255),
-  `NgaySinh` varchar(255),
-  `GioiTinh` varchar(255),
-  `DiaChi` varchar(255),
-  `SoCMND` varchar(255),
-  `SoDienThoai` varchar(255),
-  `Email` varchar(255),
-  `MaPB` varchar(255),
-  `MaCV` varchar(255),
-  `MaTDHV` varchar(255)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `phongban` (
-  `MaPB` varchar(255) PRIMARY KEY,
-  `TenPB` varchar(255),
-  `SoDienThoai` varchar(255)
-);
+--
+-- Database: `quanlynhansu1`
+--
 
-CREATE TABLE `trinhdohv` (
-  `MaTDHV` varchar(255) PRIMARY KEY,
-  `TenTDHV` varchar(255) UNIQUE
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `chitietluong` (
-  `MaLuong` varchar(255) PRIMARY KEY,
-  `LuongCB` double,
-  `HeSoLuong` double,
-  `ThuongPhat` double,
-  `TienLuong` double
-);
+--
+-- Table structure for table `chitietktkl`
+--
 
 CREATE TABLE `chitietktkl` (
-  `MaKTKL` varchar(255) PRIMARY KEY,
-  `SoTien` double,
-  `NgayQuyetDinh` varchar(255),
-  `LoaiQuyetDinh` varchar(255),
-  `NoiDung` varchar(255)
-);
+  `MaKTKL` varchar(255) NOT NULL,
+  `MaNV` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `chitietktkl` ADD FOREIGN KEY (`MaKTKL`) REFERENCES `khenthuongkyluat` (`MaKTKL`);
+--
+-- Dumping data for table `chitietktkl`
+--
 
-ALTER TABLE `chitietluong` ADD FOREIGN KEY (`MaLuong`) REFERENCES `luong` (`MaLuong`);
+INSERT INTO `chitietktkl` (`MaKTKL`, `MaNV`) VALUES
+('KT01', '001'),
+('KT02', '001'),
+('KT02', '002'),
+('KT03', '001');
 
-ALTER TABLE `luong` ADD FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+-- --------------------------------------------------------
 
-ALTER TABLE `khenthuongkyluat` ADD FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+--
+-- Table structure for table `chitietluong`
+--
 
-ALTER TABLE `nhanvien` ADD FOREIGN KEY (`MaCV`) REFERENCES `chucvu` (`MaCV`);
+CREATE TABLE `chitietluong` (
+  `MaLuong` varchar(255) NOT NULL,
+  `MaNV` varchar(255) NOT NULL,
+  `ThuongPhat` double NOT NULL DEFAULT 0,
+  `HeSoLuong` double NOT NULL,
+  `NgayLapBang` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `nhanvien` ADD FOREIGN KEY (`MaPB`) REFERENCES `phongban` (`MaPB`);
+--
+-- Dumping data for table `chitietluong`
+--
 
-ALTER TABLE `nhanvien` ADD FOREIGN KEY (`MaTDHV`) REFERENCES `trinhdohv` (`MaTDHV`);
+INSERT INTO `chitietluong` (`MaLuong`, `MaNV`, `ThuongPhat`, `HeSoLuong`, `NgayLapBang`) VALUES
+('Luong01', '001', 20000, 1.5, '2022-04-01'),
+('Luong01', '001', 50000, 1.5, '2022-05-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chucvu`
+--
+
+CREATE TABLE `chucvu` (
+  `MaCV` varchar(255) NOT NULL,
+  `TenCV` varchar(255) DEFAULT NULL,
+  `GhiChu` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khenthuongkyluat`
+--
+
+CREATE TABLE `khenthuongkyluat` (
+  `MaKTKL` varchar(255) NOT NULL,
+  `SoTien` double DEFAULT NULL,
+  `NgayQuyetDinh` date DEFAULT NULL,
+  `LoaiQuyetDinh` varchar(255) DEFAULT NULL,
+  `NoiDung` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `khenthuongkyluat`
+--
+
+INSERT INTO `khenthuongkyluat` (`MaKTKL`, `SoTien`, `NgayQuyetDinh`, `LoaiQuyetDinh`, `NoiDung`) VALUES
+('KT01', 100000, '2022-05-19', 'Thưởng', 'Giỏi'),
+('KT02', -50000, '2022-05-19', 'Phạt', 'Trễ'),
+('KT03', 20000, '2022-04-01', 'Thưởng', 'Thưởng tết');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `luong`
+--
+
+CREATE TABLE `luong` (
+  `MaLuong` varchar(255) NOT NULL,
+  `LuongCB` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `luong`
+--
+
+INSERT INTO `luong` (`MaLuong`, `LuongCB`) VALUES
+('Luong01', 5000000),
+('Luong02', 4500000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nhanvien`
+--
+
+CREATE TABLE `nhanvien` (
+  `MaNV` varchar(255) NOT NULL,
+  `HinhNV` varchar(255) DEFAULT NULL,
+  `TenNV` varchar(255) DEFAULT NULL,
+  `NgaySinh` varchar(255) DEFAULT NULL,
+  `GioiTinh` varchar(255) DEFAULT NULL,
+  `DiaChi` varchar(255) DEFAULT NULL,
+  `SoCMND` varchar(255) DEFAULT NULL,
+  `SoDienThoai` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `MaPB` varchar(255) DEFAULT NULL,
+  `MaCV` varchar(255) DEFAULT NULL,
+  `MaTDHV` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `nhanvien`
+--
+
+INSERT INTO `nhanvien` (`MaNV`, `HinhNV`, `TenNV`, `NgaySinh`, `GioiTinh`, `DiaChi`, `SoCMND`, `SoDienThoai`, `Email`, `MaPB`, `MaCV`, `MaTDHV`) VALUES
+('001', 'a', 'Hung', '2003-04-13', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('002', 'b', 'Hung', '2003-04-14', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('003', 'a', 'Hung', '2003-04-15', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('004', 'a', 'Hung', '2003-04-16', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('005', 'a', 'Hung', '2003-04-17', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('006', 'b', 'Hung', '2003-04-18', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('007', 'a', 'Hung', '2003-04-19', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('008', 'a', 'Hung', '2003-04-20', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('009', 'a', 'Hung', '2003-04-21', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('010', 'b', 'Hung', '2003-04-22', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('011', 'a', 'Hung', '2003-04-23', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('012', 'a', 'Hung', '2003-04-24', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('013', 'a', 'Hung', '2003-04-25', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('014', 'b', 'Hung', '2003-04-26', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('015', 'a', 'Hung', '2003-04-27', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('016', 'a', 'Hung', '2003-04-28', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('017', 'a', 'Hung', '2003-04-29', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('018', 'b', 'Hung', '2003-04-30', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('019', 'a', 'Hung', '2003-04-13', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('020', 'a', 'Hung', '2003-04-14', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('021', 'a', 'Hung', '2003-04-15', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('022', 'b', 'Hung', '2003-04-16', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('023', 'a', 'Hung', '2003-04-17', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('024', 'a', 'Hung', '2003-04-18', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('025', 'a', 'Hung', '2003-04-19', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('026', 'b', 'Hung', '2003-04-20', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('027', 'a', 'Hung', '2003-04-21', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('028', 'a', 'Hung', '2003-04-22', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('029', 'a', 'Hung', '2003-04-23', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('030', 'b', 'Hung', '2003-04-24', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('031', 'a', 'Hung', '2003-04-25', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('032', 'a', 'Hung', '2003-04-26', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('033', 'a', 'Hung', '2003-04-13', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('034', 'b', 'Hung', '2003-04-14', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('035', 'a', 'Hung', '2003-04-15', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('036', 'a', 'Hung', '2003-04-16', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('037', 'a', 'Hung', '2003-04-17', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('038', 'b', 'Hung', '2003-04-18', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('039', 'a', 'Hung', '2003-04-19', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('040', 'a', 'Hung', '2003-04-20', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('041', 'a', 'Hung', '2003-04-21', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('042', 'b', 'Hung', '2003-04-22', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('043', 'a', 'Hung', '2003-04-23', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('044', 'a', 'Hung', '2003-04-24', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL),
+('045', 'a', 'Hung', '2003-04-25', 'Nam', 'Quan6', '125', '06423', 'aaa', NULL, NULL, NULL),
+('046', 'b', 'Hung', '2003-04-21', 'Nữ', 'Quan1', '025', '0245', 'bbb', NULL, NULL, NULL),
+('047', 'a', 'Hung', '2003-04-22', 'Nam', 'Quan8', '420', '0576', 'bbb', NULL, NULL, NULL),
+('048', 'a', 'Hung', '2003-04-23', 'Nữ', 'Quan7', '147', '06474', 'bbb', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phongban`
+--
+
+CREATE TABLE `phongban` (
+  `MaPB` varchar(255) NOT NULL,
+  `TenPB` varchar(255) DEFAULT NULL,
+  `SoDienThoai` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trinhdohv`
+--
+
+CREATE TABLE `trinhdohv` (
+  `MaTDHV` varchar(255) NOT NULL,
+  `TenTDHV` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chitietktkl`
+--
+ALTER TABLE `chitietktkl`
+  ADD PRIMARY KEY (`MaKTKL`,`MaNV`),
+  ADD KEY `MaNV` (`MaNV`);
+
+--
+-- Indexes for table `chitietluong`
+--
+ALTER TABLE `chitietluong`
+  ADD PRIMARY KEY (`MaLuong`,`MaNV`,`NgayLapBang`),
+  ADD KEY `MaNV` (`MaNV`);
+
+--
+-- Indexes for table `chucvu`
+--
+ALTER TABLE `chucvu`
+  ADD PRIMARY KEY (`MaCV`);
+
+--
+-- Indexes for table `khenthuongkyluat`
+--
+ALTER TABLE `khenthuongkyluat`
+  ADD PRIMARY KEY (`MaKTKL`);
+
+--
+-- Indexes for table `luong`
+--
+ALTER TABLE `luong`
+  ADD PRIMARY KEY (`MaLuong`);
+
+--
+-- Indexes for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD PRIMARY KEY (`MaNV`),
+  ADD KEY `MaCV` (`MaCV`),
+  ADD KEY `MaPB` (`MaPB`),
+  ADD KEY `MaTDHV` (`MaTDHV`);
+
+--
+-- Indexes for table `phongban`
+--
+ALTER TABLE `phongban`
+  ADD PRIMARY KEY (`MaPB`);
+
+--
+-- Indexes for table `trinhdohv`
+--
+ALTER TABLE `trinhdohv`
+  ADD PRIMARY KEY (`MaTDHV`),
+  ADD UNIQUE KEY `TenTDHV` (`TenTDHV`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chitietktkl`
+--
+ALTER TABLE `chitietktkl`
+  ADD CONSTRAINT `chitietktkl_ibfk_1` FOREIGN KEY (`MaKTKL`) REFERENCES `khenthuongkyluat` (`MaKTKL`),
+  ADD CONSTRAINT `chitietktkl_ibfk_2` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+
+--
+-- Constraints for table `chitietluong`
+--
+ALTER TABLE `chitietluong`
+  ADD CONSTRAINT `chitietluong_ibfk_1` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`),
+  ADD CONSTRAINT `chitietluong_ibfk_2` FOREIGN KEY (`MaLuong`) REFERENCES `luong` (`MaLuong`);
+
+--
+-- Constraints for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD CONSTRAINT `nhanvien_ibfk_1` FOREIGN KEY (`MaCV`) REFERENCES `chucvu` (`MaCV`),
+  ADD CONSTRAINT `nhanvien_ibfk_2` FOREIGN KEY (`MaPB`) REFERENCES `phongban` (`MaPB`),
+  ADD CONSTRAINT `nhanvien_ibfk_3` FOREIGN KEY (`MaTDHV`) REFERENCES `trinhdohv` (`MaTDHV`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
